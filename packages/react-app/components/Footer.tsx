dccdb6ff7691791ae04ad0449411a5bd083cdc7c
sskiragu <skiragu@gmail.com>
2024-08-06 22:41:00 +0300

    style: update color theme to align with the new color scheme.

--- packages/react-app/components/Footer.tsx
+++ packages/react-app/components/Footer.tsx
@@ -29,14 +29,14 @@ const navigation = [
 
 export default function Footer() {
   return (
-    <footer className="bg-gypsum mt-auto border-black border-t">
+    <footer className="bg-gray-800 border-t border-gray-300 mt-auto">
       <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
         <div className="flex justify-center space-x-6 md:order-2">
           {navigation.map((item) => (
             <a
               key={item.name}
               href={item.href}
-              className="text-black hover:text-forest"
+              className="text-white hover:text-red-600"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -46,8 +46,8 @@ export default function Footer() {
           ))}
         </div>
         <div className="mt-8 md:order-1 md:mt-0">
-          <p className="text-center text-base text-black">
-            &copy; {new Date().getFullYear()} Build for Minipay.
+          <p className="text-center text-base text-white">
+            &copy; {new Date().getFullYear()} InfoMartKE. All rights reserved.
           </p>
         </div>
       </div>
